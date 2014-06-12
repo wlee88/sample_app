6e145b9d6a2adeb26e2e09af52b2c5273826bbfc
William Lee <me@william-lee.com>
2014-06-12 14:28:25 +0100

    Revert "more tests and refactoring"
    This reverts commit 873f6953f4b7c9e59897a741fd88f45131830374.

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -6,44 +6,31 @@ describe "UserPages" do
   describe "signup page" do
   	before { visit signup_path }
 
-    it { should have_content('Sign up') }
-    it { should have_title(full_title('Sign up')) }
+    it { should have_content('Create my account')}
+    it { should have_title(full_title('Sign up'))}
   end
 
   describe "signup" do
     before { visit signup_path }
+
     let(:submit) { "Create my account" }
 
     describe "with invalid information" do
       it "should not create a user" do
         expect { click_button submit}.not_to change(User, :count)
       end
-
-      describe "after submission" do
-        before { click_button submit}
-
-        it { should have_title('Sign up') }
-        it { should have_content('error') }
-      end
     end
 
     describe "with valid information" do
       before do
-        fill_in "Name",         with: "Michael Hartl"
-        fill_in "Email",        with: "michael@example.com"
+        fill_in "Name",         with: "Example user"
+        fill_in "Email",        with: "user@example.com"
         fill_in "Password",     with: "foobar"
         fill_in "user_password_confirmation", with: "foobar"
       end
 
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
-      end
-
-      describe "after saving a user" do
-        before { click_button submit }
-
-        it { should have_title(full_title("Michael Hartl")) }
-        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
       end
     end
   end
@@ -53,7 +40,7 @@ describe "UserPages" do
     before { visit user_path(user) }
 
     it { should have_content(user.name) }
-    it { should have_title(full_title(user.name)) }
+    it { should have_title(user.name) }
   end
 
 
